4590d5fa306e06dc8d41fd994d2e6fc7ae742ec3
willmcb94 <williammcburney@Williams-Air.fritz.box>
2022-03-07 15:12:16 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const { getTopics, getArticleById, updateArticleVotes, getUsers, getArticlesSorted, postComment, getComments, deleteCommentById, getEndpoints } = require('./controllers/controller');
 const { handlePsqlErrors, handleCustoms, handle500s } = require('./errors')
 app.use(express.json());
+const cors = require('cors');
 
+app.use(cors())
 app.get('/api/topics', getTopics)
 app.get('/api/users', getUsers)
 app.get('/api/articles', getArticlesSorted)
